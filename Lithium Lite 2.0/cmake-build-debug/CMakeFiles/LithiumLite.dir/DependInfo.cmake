6c4ad11f9a4349233c689111ea6934e2f3bcb2e0
NuymakStone <79128084+NuymakStone@users.noreply.github.com>
2021-09-12 10:38:15 +0800

    Add files via upload

---
+++ Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/imgui/imgui.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/imgui/imgui.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/imgui/imgui_demo.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/imgui/imgui_demo.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/imgui/imgui_draw.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/imgui/imgui_draw.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/imgui/imgui_impl_dx11.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/imgui/imgui_impl_dx11.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/imgui/imgui_impl_win32.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/imgui/imgui_impl_win32.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/imgui/imgui_tables.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/imgui/imgui_tables.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/imgui/imgui_widgets.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/imgui/imgui_widgets.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/main.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/main.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/misc/binds.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/misc/binds.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/misc/status.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/misc/status.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/combat/clicker.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/combat/clicker.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/combat/reach.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/combat/reach.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/combat/throwpot.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/combat/throwpot.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/combat/velocity.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/combat/velocity.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/destruct/destruct.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/destruct/destruct.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/movement/antiafk.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/movement/antiafk.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/movement/bhop.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/movement/bhop.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/movement/fly.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/movement/fly.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/movement/speed.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/movement/speed.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/movement/timer.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/movement/timer.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/visuals/cavefinder.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/visuals/cavefinder.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/visuals/esp.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/visuals/esp.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/modules/visuals/nametags.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/modules/visuals/nametags.cpp.obj"
+  "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/scanner/scanner.cpp" "D:/下载/谷歌下载/lithium.rip-2.0 (2)/lithium.rip-2.0/Lithium Lite 2.0 (no auth)/Lithium Lite 2.0/cmake-build-debug/CMakeFiles/LithiumLite.dir/scanner/scanner.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../imgui"
+  "../imgui/fonts"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
